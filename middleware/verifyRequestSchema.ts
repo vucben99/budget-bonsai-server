036ff64f6e737e9534829f3a5aa042a7c85c1ad1
Beni Vucsics <vucben99@gmail.com>
2023-04-16 16:26:20 +0200

    Endpoints now return Zod error object when sending bad request schema

--- middleware/verifyRequestSchema.ts
+++ middleware/verifyRequestSchema.ts
@@ -1,10 +1,11 @@
-import { safeParse } from "../utils/safeParse"
 import { NextFunction, Request, Response } from "express"
 import { z } from "zod"
 
 const verifyRequestSchema = <Schema extends z.ZodTypeAny>(schema: Schema) => (req: Request, res: Response, next: NextFunction) => {
-  const result = safeParse(schema, req.body)
-  if (!result) return res.sendStatus(400)
+  const result = schema.safeParse(req.body)
+  if (result.success === false) {
+    return res.status(400).json({ error: result.error })
+  }
   next()
 }
 
