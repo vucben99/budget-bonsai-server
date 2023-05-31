caa5de65ec3b45585ee72af93b3f7c59a422e4d1
Beni Vucsics <vucben99@gmail.com>
2023-05-31 13:59:18 +0200

    Returned to original CORS option

--- app.ts
+++ app.ts
@@ -7,13 +7,7 @@ import categories from './routes/categories'
 const app = express()
 
 // Middleware
-app.use(cors({
-  'allowedHeaders': ['Content-Type'], // headers that React is sending to the API
-  'exposedHeaders': ['Content-Type'], // headers that you are sending back to React
-  'origin': '*',
-  'methods': 'GET, POST, PUT, DELETE',
-  'preflightContinue': false
-}))
+app.use(cors())
 app.use(express.json())
 
 // Routes
